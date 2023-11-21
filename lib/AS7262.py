76a7be0ee14e15d55b1a5f7968d0705670bb4011
Kamil <kamil@ratajczyk.eu>
2023-11-21 21:07:14 +0100

    AS7262

--- lib/AS7262.py
+++ lib/AS7262.py
@@ -1,7 +1,6 @@
-from gpiozero import LED
+import RPi.GPIO as GPIO
 import smbus
 import time
-
 
 AS7262_ADDRESS = 0x49
 
@@ -9,33 +8,34 @@ AS7262_CONTROL = 0x4F
 AS7262_SLAVE_STATUS_REG = 0x2F
 AS7262_DATA_1 = 0x08
 
+I2C_BUS_NUMBER = 1
 
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(I2C_BUS_NUMBER)
 
-
-power_enable_pin = LED(17)
-
-def enable_as7262_power():
-    power_enable_pin.on()
-    time.sleep(0.1)
-
-def disable_as7262_power():
-    power_enable_pin.off()
-
-def read_as7262_data():
-    bus.write_byte_data(AS7262_ADDRESS, AS7262_CONTROL, 0x03)
+power_enable_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(power_enable_pin, GPIO.OUT)
 
 
-    while True:
-        data = bus.read_byte_data(AS7262_ADDRESS, AS7262_SLAVE_STATUS_REG)
-        if (data & 0x02) == 0x02:
-            break
+class AS7262:
+    def enable_as7262_power(self):
+        GPIO.output(power_enable_pin, GPIO.HIGH)
         time.sleep(0.1)
 
-    color_data = []
-    for i in range(6):
-        color_data.append(bus.read_byte_data(AS7262_ADDRESS, AS7262_DATA_1 + i))
+    def disable_as7262_power(self):
+        GPIO.output(power_enable_pin, GPIO.LOW)
 
-    return color_data
+    def read_as7262_data(self):
+        bus.write_byte_data(AS7262_ADDRESS, AS7262_CONTROL, 0x03)
 
+        while True:
+            data = bus.read_byte_data(AS7262_ADDRESS, AS7262_SLAVE_STATUS_REG)
+            if (data & 0x02) == 0x02:
+                break
+            time.sleep(0.1)
 
+        color_data = []
+        for i in range(6):
+            color_data.append(bus.read_byte_data(AS7262_ADDRESS, AS7262_DATA_1 + i))
+
+        return color_data
