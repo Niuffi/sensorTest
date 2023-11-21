d974e42fa4620ff6c79865681368e315bf34a88f
Kamil <kamil@ratajczyk.eu>
2023-11-21 20:57:25 +0100

    AS7262

--- lib/AS7262_controller.py
+++ lib/AS7262_controller.py
@@ -3,14 +3,15 @@ from lib.AS7262 import AS7262
 
 
 class AS7262_controller(Sensor):
+
     def read(self):
+        results = {}
         try:
             AS7262.enable_as7262_power()
-            colors = AS7262.read_as7262_data()
-            for i, color in enumerate(colors):
-                print(f"Color {i + 1}: {color}")
-
+            results = AS7262.read_as7262_data()
 
         except KeyboardInterrupt:
             AS7262.disable_as7262_power()
             pass
+
+        return results
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from lib.AS7341_controller import AS7341_controller
+from lib.AS7262_controller import AS7262_controller
 
-sensor1 = AS7341_controller()
+# sensor1 = AS7341_controller()
+# sensor1.read()
+# sensor1.read_n_times(10)
+# print(sensor1.get_results())
 
-sensor1.read()
-sensor1.read_n_times(10)
-print(sensor1.get_results())
+sensor2 = AS7262_controller()
+sensor2.read()
+sensor2.read_n_times(10)
+print(sensor2.get_results())
