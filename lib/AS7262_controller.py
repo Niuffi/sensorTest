8ce6637922e58214b545157a0b277ccb919e20ac
Kamil <kamil@ratajczyk.eu>
2023-12-01 10:07:36 +0100

    plan B

--- lib/AS7262_controller.py
+++ lib/AS7262_controller.py
@@ -5,26 +5,28 @@ from lib.AS7262 import AS7262
 class AS7262_controller(Sensor):
 
     def read(self):
-        AS7262.soft_reset()
+        obj = AS7262()
 
-        hw_type, hw_version, fw_version = AS7262.get_version()
+        obj.soft_reset()
+
+        hw_type, hw_version, fw_version = obj.get_version()
 
         print('{}'.format(fw_version))
 
-        AS7262.set_gain(64)
+        obj.set_gain(64)
 
-        AS7262.set_integration_time(17.857)
+        obj.set_integration_time(17.857)
 
-        AS7262.set_measurement_mode(2)
+        obj.set_measurement_mode(2)
 
-        # AS7262.set_illumination_led_current(12.5)
-        AS7262.set_illumination_led(1)
-        # AS7262.set_indicator_led_current(2)
-        # AS7262.set_indicator_led(1)
+        # obj.set_illumination_led_current(12.5)
+        obj.set_illumination_led(1)
+        # obj.set_indicator_led_current(2)
+        # obj.set_indicator_led(1)
 
         try:
             while True:
-                results = AS7262.get_calibrated_values()
+                results = obj.get_calibrated_values()
                 print("""
         Red:    {}
         Orange: {}
@@ -33,7 +35,7 @@ class AS7262_controller(Sensor):
         Blue:   {}
         Violet: {}""".format(*results))
         except KeyboardInterrupt:
-            AS7262.set_measurement_mode(3)
-            AS7262.set_illumination_led(0)
+            obj.set_measurement_mode(3)
+            obj.set_illumination_led(0)
 
         return results
